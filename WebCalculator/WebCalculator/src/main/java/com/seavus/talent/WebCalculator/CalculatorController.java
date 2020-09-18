154fb90c28c1b88b7615e42aa3277682bbeb8b94
Viki Ristova <viktorijaristova996@gmail.com>
2020-09-18 22:57:23 +0200

    Exercises Day 4

--- WebCalculator/WebCalculator/src/main/java/com/seavus/talent/WebCalculator/CalculatorController.java
+++ WebCalculator/WebCalculator/src/main/java/com/seavus/talent/WebCalculator/CalculatorController.java
@@ -6,20 +6,14 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class CalculatorController {
 
-
     @RequestMapping(path = "/calculate", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public String calculate(@RequestParam String operandOne, @RequestParam String operator, @RequestParam String operandTwo) {
-        String errorMsg = "";
+    public String greet(@RequestParam String operandOne, @RequestParam String operator, @RequestParam String operandTwo) {
         if (operator.equals("+")) {
             return String.valueOf(Integer.valueOf(operandOne) + Integer.valueOf(operandTwo));
         } else if (operator.equals("-")) {
             return String.valueOf(Integer.valueOf(operandOne) - Integer.valueOf(operandTwo));
-        } else if (!operator.equals("+") && !operator.equals("-")) {
-            errorMsg = "Error: An invalid operator was inserted.";
-            return errorMsg;
-        }
-        errorMsg = "An error occurred :/";
-        return errorMsg;
+        } else
+            return "An error occurred :/";
     }
 }
